6edc7a4d162f9e010ad02094fa4a3741c6ae594f
alex <gorrilla86@gmail.com>
2019-10-19 08:32:12 -0600

    Adding max depth recursive lesson

--- learn/binary_tree/solve_problems_recursively/maximum_depth_of_binary_tree.py
+++ learn/binary_tree/solve_problems_recursively/maximum_depth_of_binary_tree.py
@@ -1,21 +1,22 @@
-#Maximum Depth of Binary Tree
-#Solution
-#Given a binary tree, find its maximum depth.
+# Maximum Depth of Binary Tree
+# Solution
+# Given a binary tree, find its maximum depth.
 #
-#The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+# The maximum depth is the number of nodes along the longest 
+# path from the root node down to the farthest leaf node.
 #
-#Note: A leaf is a node with no children.
+# Note: A leaf is a node with no children.
 #
-#Example:
+# Example:
 #
-#Given binary tree [3,9,20,null,null,15,7],
+# Given binary tree [3,9,20,null,null,15,7],
 #
 #    3
 #   / \
 #  9  20
 #    /  \
 #   15   7
-#return its depth = 3.
+# return its depth = 3.
 
 # Definition for a binary tree node.
 # class TreeNode(object):
@@ -24,9 +25,43 @@
 #         self.left = None
 #         self.right = None
 
+import sys
+sys.path.append('..')
+from binary_tree import BinaryTree
+
 class Solution(object):
     def maxDepth(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
+
+        if not root:
+        	return 0
+
+        # As we recursively call maxDepth, we are adding 1 + 1n, where n
+        # is the number of times either root.left or root.right exists at
+        # each recursive call.
+        #
+        # The max() function then decides which branch is greater, or in
+        # other words, which branch has more depth.
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+def main():
+	
+	# Just building the tree pictured above.
+	example_tree = BinaryTree(3)
+	example_tree.insertLeft(9)
+	example_tree.insertRight(7)
+	example_tree.insertRight(20)
+	new_node = example_tree.getright()
+	new_node.insertLeft(15)
+	new_node.insertLeft(10)
+	
+	print Solution().maxDepth(example_tree)
+
+	
+
+main()
